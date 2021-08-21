57ffdf92a85f289bec86d21b593346614ddc2844
praveenakupp <88589719+praveenakupp@users.noreply.github.com>
2021-08-21 22:53:29 +0530

    Add files via upload

--- Students.java
+++ Students.java
@@ -1,4 +1,28 @@
 
 public class Students {
 
-}
+	public void getStudentInfo(int id)
+	{
+		
+	}
+	public void getStudentInfo(int id,String name)
+	{
+		
+	}
+	public void getStudentInfo(String email,int phonenumber)
+	{
+		
+	}
+	public static void main(String[] args)
+	{
+		Students sd=new Students();
+		sd.getStudentInfo(99);
+		sd.getStudentInfo(99, "PK");
+		sd.getStudentInfo("pk@gmail.com", 267676273);
+		System.out.println("My Id is: " + 99);
+		System.out.println("My name is:" + "PK");
+		System.out.println("My Email id is:" + "pk@gmail.com");
+		System.out.println("My Phone number is:" + 267676273);
+	}
+	}
+
